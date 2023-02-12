87122941239dfe94edee7c5de2b967dbb2e218c6
Jeremías Rivelli <jeremias.j.riv@gmail.com>
2023-02-12 20:06:34 -0300

    Ejercicios resueltos hasta el 4 extras

---
+++ IntroJava/src/Ejercicios/Extras/Ejercicio5Extras.java
@@ -0,0 +1,17 @@
+package Ejercicios.Extras;
+//Una obra social tiene tres clases de socios:
+//o Los socios tipo ‘A’ abonan una cuota mayor, pero tienen un 50% de descuento en
+//todos los tipos de tratamientos.
+//o Los socios tipo ‘B’ abonan una cuota moderada y tienen un 35% de descuento para
+//los mismos tratamientos que los socios del tipo A.
+//o Los socios que menos aportan, los de tipo ‘C’, no reciben descuentos sobre dichos
+//tratamientos.
+//o Solicite una letra (carácter) que representa la clase de un socio, y luego un valor
+//real que represente el costo del tratamiento (previo al descuento) y determine el
+//importe en efectivo a pagar por dicho socio.
+
+public class Ejercicio5Extras {
+    public static void main(String[] args) {
+
+    }
+}
