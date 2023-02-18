e4742a32581ba94ef16fa740b831bd628c52773d
Jeremías Rivelli <jeremias.j.riv@gmail.com>
2023-02-18 18:12:07 -0300

    Agregado el español

--- JavaPOO/ejercicio3/src/entidad/Operacion.java
+++ JavaPOO/ejercicio3/src/entidad/Operacion.java
@@ -1,17 +1,17 @@
 package entidad;
-//Crear una clase llamada Operacion que tenga como atributos privados numero1 y numero2.
-//A continuación, se deben crear los siguientes métodos:
+//Crear una clase llamada Operación que tenga como atributos privados numero1 y numero2.
+//A continuación, se deben hacer los siguientes métodos:
 //a) Método constructor con todos los atributos pasados por parámetro.
-//b) Metodo constructor sin los atributos pasados por parámetro.
+//b) Método constructor sin los atributos pasados por parámetro.
 //c) Métodos get y set.
-//d) Método para crearOperacion(): que le pide al usuario los dos números y los guarda
+//d) Método para crearOperación(): que le pide al usuario los dos números y los guarda
 //en los atributos del objeto.
 //e) Método sumar(): calcular la suma de los números y devolver el resultado al main.
 //f) Método restar(): calcular la resta de los números y devolver el resultado al main
-//g) Método multiplicar(): primero valida que no se haga una multiplicación por cero, si
+//g) Método multiplicar(): primero válida que no se haga una multiplicación por cero, si
 //fuera a multiplicar por cero, el método devuelve 0 y se le informa al usuario el error.
 //Si no, se hace la multiplicación y se devuelve el resultado al main
-//h) Método dividir(): primero valida que no se haga una división por cero, si fuera a pasar
+//h) Método dividir(): primero válida que no se haga una división por cero, si fuera a pasar
 //una división por cero, el método devuelve 0 y se le informa al usuario el error se le
 //informa al usuario. Si no, se hace la división y se devuelve el resultado al main.
 public class Operacion {
