d4c6aaa083fb88d31fe23e1cf7aeb28029475bd0
Jeremías Rivelli <jeremias.j.riv@gmail.com>
2023-02-27 13:11:15 -0300

    Cambios ejercicio 15

--- introjava/src/ejercicios/vectoresymatrices/Ejercicio15.java
+++ introjava/src/ejercicios/vectoresymatrices/Ejercicio15.java
@@ -6,6 +6,16 @@ public class Ejercicio15 {
     public static void main(String[] args) {
         int[] vector = new int[100];
         int numeros = 0;
+        int aux;
+        for (int i = 0; i < vector.length - 1; i++) {
+            for (int j = i + 1; j < vector.length; j++) {
+                if (vector[i] > vector[j]) {
+                    aux = vector[i];
+                    vector[i] = vector[j];
+                    vector[j] = aux;
+                }
+            }
+        }
         for (int i = 0; i < vector.length; i++) {
             vector[i] = numeros++;
             System.out.println(vector[i] + 1);
