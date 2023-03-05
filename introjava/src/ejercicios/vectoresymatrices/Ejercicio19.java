208163456e0b891498f810dd42c0d81ed2078dc2
Jeremías Rivelli <jeremias.j.riv@gmail.com>
2023-03-05 16:17:05 -0300

    Ejercicio19 Modificado

--- introjava/src/ejercicios/vectoresymatrices/Ejercicio19.java
+++ introjava/src/ejercicios/vectoresymatrices/Ejercicio19.java
@@ -2,9 +2,9 @@ package ejercicios.vectoresymatrices;
 
 import java.util.Random;
 
-//Realice un programa que compruebe si una matriz dada es anti simétrica. Se dice que una
-//matriz A es anti simétrica cuando ésta es igual a su propia traspuesta, pero cambiada de
-//signo. Es decir, A es anti simétrica si A = -AT. La matriz traspuesta de una matriz A se
+//Realice un programa que compruebe si una matriz dada es antisimétrica. Se dice que una
+//matriz A es antisimétrica cuando esta es igual a su propia traspuesta, pero cambiada de
+//signo. Es decir, A es antisimétrica si A = -AT. La matriz traspuesta de una matriz A se
 //denota por AT y se obtiene cambiando sus filas por columnas (o viceversa)
 public class Ejercicio19 {
     public static void main(String[] args) {
@@ -12,15 +12,15 @@ public class Ejercicio19 {
         int[][] matrizB = new int[4][4];
 
         Random claseRandom = new Random();
-
+        int randomInt;
         System.out.println("MATRIZ A");
         System.out.println();
         for (int i = 0; i < matrizA.length; i++) {
             for (int j = 0; j < matrizA.length; j++) {
-                int randomInt = -1 + claseRandom.nextInt(1 + 1);
+                randomInt = -1 + claseRandom.nextInt((int) (2 + (10 * Math.pow(5, Math.random() * 5))));
                 matrizA[i][j] = randomInt;
-                if (matrizA[i][j] >= 0) {
-                    System.out.print("+" + matrizA[i][j] + " ");
+                if (j != 0) {
+                    System.out.print("+ " + matrizA[i][j] + " ");
                 } else {
                     System.out.print(matrizA[i][j] + " ");
                 }
@@ -57,8 +57,8 @@ public class Ejercicio19 {
         System.out.println();
         for (int[] ints : matrizB) {
             for (int j = 0; j < matrizB.length; j++) {
-                if (ints[j] >= 0) {
-                    System.out.print("+" + ints[j] + " ");
+                if (j != 0) {
+                    System.out.print("+ " + ints[j] + " ");
                 } else {
                     System.out.print(ints[j] + " ");
                 }
