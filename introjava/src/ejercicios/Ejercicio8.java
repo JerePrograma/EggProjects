b20d3c136d7b9b4b1471da8e141da3500a791752
RivelliDev <jeremias.j.riv@gmail.com>
2023-03-07 12:24:03 -0300

    Cambios menores

--- introjava/src/ejercicios/Ejercicio8.java
+++ introjava/src/ejercicios/Ejercicio8.java
@@ -3,9 +3,9 @@ package ejercicios;
 import java.util.Scanner;
 
 //Realizar un programa que solo permita introducir solo frases o palabras de 8 de largo. Si el
-//        usuario ingresa una frase o palabra de 8 de largo se deberá de imprimir un mensaje por
-//        pantalla que diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”.
-//        Nota: investigar la función Lenght() en Java.
+//usuario ingresa una frase o palabra de 8 de largo se deberá de imprimir un mensaje por
+//pantalla que diga “CORRECTO”, en caso contrario, se deberá imprimir “INCORRECTO”.
+//Nota: investigar la función Lenght() en Java.
 public class Ejercicio8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
